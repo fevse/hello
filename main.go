c6d6baec24d39f4b73160a287d771959b5ba28ab
fevse <fedulov.v.s@gmail.com>
2023-06-11 23:39:14 +0600

    update router

--- main.go
+++ main.go
@@ -6,27 +6,22 @@ import (
 	"strings"
 )
 
-type MyHandler struct {
-}
-
-func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		w.Write([]byte("Home"))
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/hello/") {
-		name := strings.Split(r.URL.Path, "/")[2]
-		w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
-		return
-	}
-
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("404 Page Not Found"))
-
-}
-
 func main() {
-	http.ListenAndServe(":3000", MyHandler{})
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/hello/", hello)
+
+	http.ListenAndServe(":3000", mux)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home"))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := strings.Split(r.URL.Path, "/")[2]
+	w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
 }
