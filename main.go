2d338be58a764d6135a7cb0f7f436ccd585da773
fevse <fedulov.v.s@gmail.com>
2023-06-13 20:46:13 +0600

    add handeler404

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -18,10 +19,24 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		handler404(w, r)
+		return
+	}
 	w.Write([]byte("Home"))
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	pathRegexp := regexp.MustCompile(`/hello/\w+$`)
+	if !pathRegexp.Match([]byte(r.URL.Path)) {
+		handler404(w, r)
+		return
+	}
 	name := strings.Split(r.URL.Path, "/")[2]
 	w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
+}
+
+func handler404(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Page Not Found"))
 }
